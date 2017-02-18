cc02c57b494dddec908e0cd9c3ed811566b4f417
Florian Weik <fweik@icp.uni-stuttgart.de>
2017-02-18 20:27:21 +0100

    Random access bm, variable lengths.

--- flat_map_bm.cpp
+++ flat_map_bm.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <random>
-#include <vector>
 #include <unordered_map>
+#include <vector>
+
+#include <iostream>
 
 #include <benchmark/benchmark.h>
 
@@ -15,26 +17,106 @@ std::vector<value_type> random_vector(std::size_t n) {
   std::mt19937 mersenne_engine(n);
   std::uniform_int_distribution<int> dist;
 
-  std::transform(ret.begin(), ret.end(), ret.begin(), [&dist, &mersenne_engine](value_type) -> value_type {
-							return {dist(mersenne_engine), dist(mersenne_engine)};
-						      });
-  
+  std::transform(ret.begin(), ret.end(), ret.begin(),
+                 [&dist, &mersenne_engine](value_type) -> value_type {
+                   return {dist(mersenne_engine), dist(mersenne_engine)};
+                 });
+
   return ret;
 }
 
-
-template<typename Container>
+template <typename Container>
 static void BM_fill_random(benchmark::State &state) {
-  auto values = random_vector(10000);
-  
-  while(state.KeepRunning()) {
-    //    benchmark::DoNotOptimize(Container(values.begin(), values.end()));
-  }  
+  auto values = random_vector(state.range(0));
+
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(Container(values.begin(), values.end()));
+  }
 }
 
-BENCHMARK_TEMPLATE(BM_fill_random, std::unordered_map<int, int>);
-BENCHMARK_TEMPLATE(BM_fill_random, boost::container::flat_map<int, int>);
+template <typename Container>
+static void BM_fill_ordered(benchmark::State &state) {
+  auto values = random_vector(state.range(0));
+  std::sort(values.begin(), values.end(),
+            [](value_type const &a, value_type const &b) {
+              return a.first < b.first;
+            });
 
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(Container(values.begin(), values.end()));
+  }
+}
+
+template <typename Container>
+static void BM_sort_and_fill(benchmark::State &state) {
+  auto values = random_vector(state.range(0));
+  std::sort(values.begin(), values.end(),
+            [](value_type const &a, value_type const &b) {
+              return a.first < b.first;
+            });
+
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(Container(values.begin(), values.end()));
+  }
+}
+
+template <typename Container>
+static void BM_traversal(benchmark::State &state) {
+  auto values = random_vector(state.range(0));
+  Container map(values.begin(), values.end());
+
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(std::accumulate(
+        map.begin(), map.end(), 0,
+        [](int s, value_type const &e) { return s + e.second; }));
+  }
+}
+
+template <typename Container>
+static void BM_random_access(benchmark::State &state) {
+  auto values = random_vector(state.range(0));
+
+  int i = 0;
+  for (auto &e : values) {
+    e.first = i++;
+  }
+
+  Container map(values.begin(), values.end());
+
+  std::mt19937 mersenne_engine(state.range(0));
+  std::uniform_int_distribution<int> dist(0, state.range(0) - 1);
+
+  int sum = 0;
+  while (state.KeepRunning()) {
+    auto const id = dist(mersenne_engine);
+
+    benchmark::DoNotOptimize(sum += map.at(id));
+  }
+}
+
+BENCHMARK_TEMPLATE(BM_random_access, std::unordered_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_random_access, boost::container::flat_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_fill_random, std::unordered_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_fill_random, boost::container::flat_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_fill_ordered, std::unordered_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_fill_ordered, boost::container::flat_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_traversal, std::unordered_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
+BENCHMARK_TEMPLATE(BM_traversal, boost::container::flat_map<int, int>)
+    ->RangeMultiplier(2)
+    ->Range(8, 8 << 10);
 
 BENCHMARK_MAIN();
-
